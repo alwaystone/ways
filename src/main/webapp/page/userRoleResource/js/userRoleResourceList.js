2cb8391adba3baae61dcf3f96dd89d280088e607
alwaystone <30831447+alwaystone@users.noreply.github.com>
2017-08-10 16:35:47 +0800

    列表展示

--- src/main/webapp/page/userRoleResource/js/userRoleResourceList.js
+++ src/main/webapp/page/userRoleResource/js/userRoleResourceList.js
@@ -8,31 +8,41 @@ $(function(){
 		striped:true,  
 		collapsible:true,  
 		toolbar:"#easyui_toolbar",  
+		//url: "/ways/userController/getUserList",
 		//url:'/ways/userController/getUserList', //搜索前,触发此action请求所有用户信息  
 		loadMsg:'数据加载中......',  
 		fitColumns:true,//允许表格自动缩放,以适应父容器  
 		sortOrder:'asc',  
 		remoteSort:false,  
+		height:"100%",
 	 	columns : [ [ 
-	 		{field:'code',title:'编号',width:100},
+	 		{field:'userCode',title:'编号',width:100},
 			{field:'name',title:'姓名',width:100},
 			{field:'berthday',title:'生日',width:100},
 			{field:'phone',title:'电话',width:100},
 			{field:'address',title:'地址',width:100},
-			{field:'deptName',title:'所属部门',width:100} 
+			{field:'deptName',title:'所属部门',width:100},
+			{field:'opt',title:'操作',width:100,
+				formatter:function(value,rec){
+					return '<a href="#" class="easyui-linkbutton" plain="true" icon="icon-add">查看</a>&nbsp;&nbsp;'+
+					'<a href="#" class="easyui-linkbutton" plain="true" icon="icon-add">编辑</a>&nbsp;&nbsp;'+
+					'<a href="#" class="easyui-linkbutton" plain="true" icon="icon-add">删除</a>';
+				}
+			}
 		] ],  
 	    pagination : true,  
 	    rownumbers : true  
 	}); 
 	
 	function loadData(){
+		///$('#userRoleResourceGrid').datagrid('/ways/userController/getUserList');
 		$.ajax({
             type: "post",
             url: "/ways/userController/getUserList",
             data: {},
             dataType: "json",
             success: function(data){
-            	$('#userRoleResourceGridData').datagrid('loadData',data);  
+            	$('#userRoleResourceGrid').datagrid('loadData',data);   
              }
         });
 	}
